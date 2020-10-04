7ca15c308dbaefa3c1adacdbdffafbdf80f68b65
Vikas <47245129+Vikas-Tamboli@users.noreply.github.com>
2020-10-04 18:28:01 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,14 +1,9 @@
-provider "aws" {
-	profile = "jenkins"
-	region = "ap-south-1"
-}
-
 terraform{
 	backend "s3"{
     
-		bucket = "mybucket880"
-		key = "infra/terraform.tfstate"
-		region = "ap-south-1"
-
+		bucket  = "mybucket880"
+		key     = "infra/terraform.tfstate"
+		region  = "ap-south-1"
+		profile = "jenkins"
 }
 }
