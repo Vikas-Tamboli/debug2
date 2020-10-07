2100648cfa8c8402aed3fa01b73624952ffa3e9f
root <root@vikas.localdomain>
2020-10-08 00:09:16 +0530

    update backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform{
 	backend "s3"{
     
 		bucket  = "mybucket880"
-		key     = "infra/terraform.tfstate"
+		key     = "vik/terraform.tfstate"
 		region  = "ap-south-1"
 		profile = "jenkins"
 }
