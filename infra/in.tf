02f5017eb17e024c1cabe1e4eac935dabaecd55a
root <root@vikas.localdomain>
2020-10-07 23:48:57 +0530

    bbb

--- infra/in.tf
+++ infra/in.tf
@@ -8,21 +8,21 @@ resource "aws_security_group" "test_sg" {
 	from_port = var.ssh_port
 	to_port = var.ssh_port
 	protocol = "tcp"
-	cidr_blocks ="${ var.ssh_cidr}"
+	cidr_blocks = [0.0.0.0/0]
 	}
 
 	ingress {
         from_port = var.http_port
         to_port = var.http_port
         protocol = "tcp"
-        cidr_blocks = "${var.cidr}"
+        cidr_blocks = [0.0.0.0/0]
         }
 	
 	egress {
 	from_port = 0
 	to_port = 0
 	protocol = "-1"
-	cidr_blocks = "${var.cidr}"
+	cidr_blocks = [0.0.0.0/0]
         }
 	tags={
 		Name = var.sg_tag
